d2b9b12f5803c5a0da64a25a16d8bb84173dd27b
Troy <tydamon@hotmail.com>
2024-07-21 02:59:31 +1000

    removed some apollo and graphql things

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,48 +4,23 @@ import { Outlet } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 // subjects //
 
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <>
-        <header className="header">
-          <Header />
-          <Navigation />
-        </header>
+    <>
+      <header className="header">
+        <Header />
+        <Navigation />
+      </header>
 
-        <div className="bucket-app">
+      <div className="bucket-app">
+        <Outlet />
+      </div>
 
-            <Outlet />
-
-        </div>
-
-        <Footer />
-      </>
-    </ApolloProvider>
+      <Footer />
+    </>
   );
 }
 
